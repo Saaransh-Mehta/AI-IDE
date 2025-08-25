194fd16a9ab668f8b1de3896e5e022cd3f511c4d
Saaransh-Mehta <saaransh1621m@gmail.com>
2025-08-25 23:00:55 +0530

    next auth configuration setup creating custom and modular next auth scalable system

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,8 @@ import {
 } from '@/routes'
 
 import authConfig from "./auth.config";
+import { redirect } from "next/dist/server/api-utils";
+import { url } from "inspector";
 
 
 const {auth} = NextAuth(authConfig)
@@ -16,5 +18,30 @@ const {auth} = NextAuth(authConfig)
 export default auth((req)=>{
     const {nextUrl} = req
     const isLoggedIn = !!req.auth
-    
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+    const isPublic = publicRoutes.includes(nextUrl.pathname)
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+
+
+    if(isApiAuthRoute){
+         return null
+    }
+    if(isAuthRoute){
+        if(isLoggedIn){
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT,nextUrl))
+        }
+        return null
+    }
+
+    if(!isLoggedIn && !isPublic){
+        return Response.redirect(new URL("/auth/signin",nextUrl))
+    }
+return null
 })
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|auth).*)",
+  ],
+};
---
+++ module/auth/actions/index.ts
@@ -0,0 +1,34 @@
+"use server"
+import { auth } from "@/auth"
+import { db } from "@/lib/db"
+ export const getUserById = async(id:string)=>{
+    try{
+        const user = await db.user.findUnique({
+            where:{id},
+            include:{
+                accounts:true
+            }
+        })
+        return user
+    }catch(error){
+        console.log(error + "Error at index.ts in action folder")
+        return null
+    }
+ }
+
+ export const getAccountById = async(userId: string)=>{
+    try{
+        const accountDetail = await db.account.findFirst({
+            where:{userId}
+        })
+        return accountDetail
+    }catch(error){
+        console.log(error + "Error at index.ts under action in auth folder")
+        return null
+    }
+ }
+
+ export const currectUser = async()=>{
+    const user = await auth()
+    return user?.user;
+ }
